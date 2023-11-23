85977274138b370945b1f881e2ef353234b8fd69
mariusdanbmd <34275284+mariusdanbmd@users.noreply.github.com>
2023-11-23 17:25:07 +0200

    initial implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = lorem.sentence();
+  enteredText = '';
+  // solved = false;
+
+  onInput(event: Event) {
+    console.log((<HTMLInputElement>event.target).value);
+    this.enteredText = (<HTMLInputElement>event.target).value;
+
+    // another way to do it
+    // if ((<HTMLInputElement>event.target).value === this.randomText)
+    //   this.solved = true
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+
+    return enteredLetter === randomLetter ? 'correct' : 'incorrect';
+  }
 }
